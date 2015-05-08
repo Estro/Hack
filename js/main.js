8deefa5c1c85eb108f8453db31b93f01e1e6ef0b
Martin <martinpomeroy@outlook.com>
2015-05-08 04:09:33 +0100

    zxczxc

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
      "use strict";
 
      var room = Math.random().toString(36).substring(7);
-     $('.player iframe').attr('src', 'https://apprtc.appspot.com/r/Tesco'+ room);
+     $('.player iframe').attr('src', 'https://apprtc.appspot.com/r/Tesco' + room);
 
      TESCO.gotoScreen = function(screen) {
          $('.screen').hide();
@@ -18,10 +18,12 @@
          $('#startButton').click(function() {
              $('.selection').hide();
              $('.player').show();
-              socket.emit('connected', room);
+             socket.emit('connected', room);
          });
 
          $('.tab-item').click(function() {
+             $('.loader').show();
+             $('.desktop').attr('src', '');
              $('.tab-item').removeClass('active');
              $(this).addClass('active');
              if ($(this).text().trim() === 'Home') {
@@ -37,25 +39,25 @@
              TESCO.gotoScreen('selection');
          });
 
-         $('#send-btn').click(function (){
-         	var text = $('#sender').val();
-         	 socket.emit('message', text);
-         	 console.log('sendings');
-         	 $('#sender').val('');
-         	 $('.shopping-list').append('<div class="list-item"><img src="http://www.americansweets.co.uk/ekmps/shops/statesidecandy/images/walkers-prawn-cocktail-flavour-crisps-case-of-48-x-32.5g-bags-6107-p.jpg"><h2>New Item</h2><h3>£1.50</h3><span>1</span></div>');
+         $('#send-btn').click(function() {
+             var text = $('#sender').val();
+             socket.emit('message', text);
+             console.log('sendings');
+             $('#sender').val('');
+             $('.shopping-list').append('<div class="list-item"><img src="http://www.americansweets.co.uk/ekmps/shops/statesidecandy/images/walkers-prawn-cocktail-flavour-crisps-case-of-48-x-32.5g-bags-6107-p.jpg"><h2>New Item</h2><h3>£1.50</h3><span>1</span></div>');
 
          });
 
-         $('.tabs li').click(function (){
-         	$('.tab-content').hide();
-         	var tab = $(this).text().trim();
-         	if (tab === 'Shopping List'){
-         		$('.tab-content.two').show();
-         	}
+         $('.tabs li').click(function() {
+             $('.tab-content').hide();
+             var tab = $(this).text().trim();
+             if (tab === 'Shopping List') {
+                 $('.tab-content.two').show();
+             }
 
-         	if (tab === 'System Links'){
-         		$('.tab-content.three').show();
-         	}
+             if (tab === 'System Links') {
+                 $('.tab-content.three').show();
+             }
          });
      };
      var socket = io.connect();
@@ -64,17 +66,17 @@
          $('.user-message').text(message).fadeIn();
          $('.shopping-list').append('<div class="list-item"><img src="http://www.americansweets.co.uk/ekmps/shops/statesidecandy/images/walkers-prawn-cocktail-flavour-crisps-case-of-48-x-32.5g-bags-6107-p.jpg"><h2>New Item</h2><h3>£1.50</h3><span>1</span></div>');
 
-         setTimeout(function (){
-         	$('.user-message').fadeOut();
+         setTimeout(function() {
+             $('.user-message').fadeOut();
          }, 5000);
 
      });
 
 
      socket.on('connected', function(message) {
-     	console.log('received');
-        $('.loader').hide();
- 		$('.desktop').attr('src', 'https://apprtc.appspot.com/r/Tesco'+ message);
+         console.log('received');
+         $('.loader').hide();
+         $('.desktop').attr('src', 'https://apprtc.appspot.com/r/Tesco' + message);
      });
 
      TESCO.events();
