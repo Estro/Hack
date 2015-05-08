1770338855e227e5c578053a47b2b0adb0123a27
Martin <martinpomeroy@outlook.com>
2015-05-08 03:13:27 +0100

    ssdasdas

--- server.js
+++ server.js
@@ -7,36 +7,14 @@ var app = http.createServer(function(req, res) {
 
 var io = require('socket.io').listen(app);
 io.sockets.on('connection', function(socket) {
-io.configure(function () {
-  io.set("transports", ["xhr-polling"]);
-  io.set("polling duration", 3);
-});
 
     socket.on('message', function(message) {
         socket.broadcast.emit('message', message);
+        console.log(message);
     });
 
-   socket.on('chat', function(message) {
-        socket.broadcast.emit('chat', message);
-    });
-
-    socket.on('create or join', function(room) {
-        var numClients = io.sockets.clients(room).length;
-
-        if (numClients === 0) {
-            socket.join(room);
-            socket.emit('created', room);
-        } else if (numClients == 1) {
-
-            io.sockets. in (room).emit('join', room);
-            socket.join(room);
-            socket.emit('joined', room);
-        } else {
-            socket.emit('full', room);
-        }
-
-        socket.emit('emit(): client ' + socket.id + ' joined room ' + room);
-        socket.broadcast.emit('broadcast(): client ' + socket.id + ' joined room ' + room);
+    socket.on('connected', function(message) {
+        socket.broadcast.emit('connected');
 
     });
 
